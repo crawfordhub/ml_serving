4b3c66fbc10c6f3d759eabada024f0a54e168788
crawfordhub <dcrawford.j@gmail.com>
2020-11-28 13:48:09 -0800

    bug fix

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, jsonify
+import numpy as np
 
 app = Flask(__name__)
 
 def run_request():
     index = int(request.json['index'])
     list = ['red', 'green', 'blue', 'yellow', 'black']
+    if index < 0 or index > len(list):
+        return np.random.randn()
     return list[index]
 
 @app.route('/', methods=['GET', 'POST'])
